82c551b7b293cca30aad79eddca8798b5c142b70
LiuSomeoneJava <117787147+LiuSomeoneJava@users.noreply.github.com>
2022-11-10 10:42:43 +0800

    push test3

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -11,6 +11,7 @@ public class GitTest {
         System.out.println("分子冲突 master");
         System.out.println("push test");
         System.out.println("push test2");
+         System.out.println("push test3");
     }
 
 }
