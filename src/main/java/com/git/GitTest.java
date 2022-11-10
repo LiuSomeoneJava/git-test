ad3cf9232a031d2a5443274dd1dfeca8fa33ef41
admin <645297261@qq.com>
2022-11-10 10:30:46 +0800

    push test commit

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -9,7 +9,7 @@ public class GitTest {
         System.out.println("hot-fix");
         System.out.println("分支冲突 hot-fix");
         System.out.println("分子冲突 master");
-
+        System.out.println("push test");
     }
 
 }
